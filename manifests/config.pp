03ec0dd4274e095fb2eefb8a15955ca18760daa3
root <root@puppet.rusu.lab>
2021-09-06 06:26:39 +0000

    Config file class added

---
+++ manifests/config.pp
@@ -0,0 +1,13 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include apache::config
+class apache::config {
+  file { 'apache_config':
+    ensure => $apache::config_ensure,
+    path   => $apache::config_path,
+    source => "puppet:///modules/apache/${osfamily}.conf"
+  }
+}
