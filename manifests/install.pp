fbfb8476a2040128c990750cba294685780a4e4b
root <root@puppet.rusu.lab>
2021-09-06 04:48:16 +0000

    Refactored install class for Debian family servers

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,8 +4,11 @@
 #
 # @example
 #   include apache::install
-class apache::install {
-  package { 'apache2':
-    ensure => 'present',
+class apache::install (
+  $install_name   = $apache::params::install_name,
+  $install_ensure = $apache::params::install_ensure,
+) inherits apache::params {
+  package { "${install_name}":
+    ensure => $install_ensure,
   }
 }
